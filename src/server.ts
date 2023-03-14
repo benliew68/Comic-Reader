b6653681ecfba22efde35bbc264da4247bcbe55b
Ben Liew <benliew68@gmail.com>
2023-03-14 20:32:43 +1100

    Created stubs for comic-reader based on legacy python code and installed React with boilerplate code

---
+++ src/server.ts
@@ -0,0 +1,95 @@
+import express, { json, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+// import * as CryptoJS from 'crypto-js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 5002;
+const IP = '127.0.0.1';
+
+app.get('/', (req: Request, res: Response) => {
+    console.log('Root URL Accessed!');
+    // Redirect to login
+    res.json(req.query);
+});
+
+app.route('/login')
+.post((req: Request, res: Response) => {
+    res.json({});
+})
+.get((req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/logout', (req: Request, res: Response) => {
+    // Maybe change to post
+    res.json({});
+});
+
+app.get('/profile', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.route('/settings')
+.get((req: Request, res: Response) => {
+    res.json({});
+})
+.post((req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/removecomic', (req: Request, res: Response) => {
+    // Maybe change to delete?
+    res.json({});
+});
+
+app.get('/library', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/search', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/comicinfo', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/read', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/user', (req: Request, res: Response) => {
+    res.json({});
+});
+
+// app.get('/viewusers', (req: Request, res: Response) => {
+//     res.json({});
+// });
+
+app.get('/offline.html', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/service-worker.js', (req: Request, res: Response) => {
+    res.json({});
+});
+
+app.get('/test_api', (req: Request, res: Response) => {
+    res.send({ "users": ["userOne", "userTwo", "userThree"] });
+});
+
+app.use(morgan('dev'));
+
+const server = app.listen(PORT, IP, () => {
+    console.log(`Server running on http://${IP}:${PORT}`);
+    // Initialise database
+});
+
+process.on('SIGINT', () => {
+    // Clear any setIntervals or any setTimeouts or any remaining processes
+    server.close(() => console.log('Server closed'));
+});
